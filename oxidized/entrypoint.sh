f1d71cbd4abe65b5785fa6794780e3ef37d14f07
Denis Rykov <drycov@gmail.com>
2026-03-04 22:35:17 +0500

    Обновить конфигурацию Oxidized: изменить пути к директориям и файлам, обновить зависимости в Gemfile, изменить Dockerfile для установки необходимых пакетов и создания пользователя oxidized

--- oxidized/entrypoint.sh
+++ oxidized/entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
+set -eu
 
-CONFIG_DIR=/root/.config/oxidized
-ROOT_DIR=/root
+CONFIG_DIR="/home/oxidized/.config/oxidized"
+SSH_DIR="/home/oxidized/.ssh"
 
-mkdir -p $CONFIG_DIR
+mkdir -p "$CONFIG_DIR"
+mkdir -p "$SSH_DIR"
 
-cp /opt/oxidized/config $CONFIG_DIR/config
-cp /opt/oxidized/router.db $CONFIG_DIR/router.db
-cp /opt/oxidized/ssh/config $ROOT_DIR/.ssh/config
+# config обязателен
+if [ -f /home/oxidized/config ]; then
+  cp /home/oxidized/config "$CONFIG_DIR/config"
+fi
 
-exec oxidized
+# router.db может быть либо в образе, либо примонтирован в CONFIG_DIR
+if [ -f /home/oxidized/router.db ]; then
+  cp /home/oxidized/router.db "$CONFIG_DIR/router.db"
+fi
+
+# ssh config опционально
+if [ -f /home/oxidized/ssh_config ]; then
+  cp /home/oxidized/ssh_config "$SSH_DIR/config"
+fi
+
+# Запуск через bundler: гарантирует наличие бинаря
+exec bundle exec oxidized
